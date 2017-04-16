b532ca046f04d27d27e068ab05b471179c01b028
gbaldeck <gbaldeck@gmail.com>
2017-04-15 19:41:37 -0700

    Almost ready to start the framework!

--- src/main/kotlin/org/learning/DSLconstruction.kt
+++ src/main/kotlin/org/learning/DSLconstruction.kt
@@ -1,5 +1,8 @@
 package org.learning
 
+import kotlinx.html.*
+import kotlinx.html.stream.createHTML
+
 /**
  * Created by gbaldeck on 4/11/2017.
  */
@@ -70,13 +73,254 @@ inline fun <T> T.apply(block: T.() -> Unit): T {
 //something completely different
 inline fun <T, R> with(receiver: T, block: T.() -> R): R = receiver.block()
 
-//If you don't care about the result 'apply' and 'with' can be used interchangeably
+
 fun applyWithTest() {
   val map = mutableMapOf(1 to "one")
 
-  map.apply { this[2] = "two" }
-  with(map) { this[3] = "three" }
+  map.apply { this[2] = "two" } //returns the receiver ('map' in this case)
+  with(map) { this[3] = "three" } //returns the result of the lambda after running operations on the receiver
+  //If you don't care whats returned they are interchangeable
 
   println(map)
-//  {1=one, 2=two, 3=three}
+// prints  {1=one, 2=two, 3=three}
+}
+
+//Here is an example of an HTML DSL
+fun createSimpleTable() = createHTML().
+  table {
+    tr {
+      td { +"cell" }
+    }
+  }
+
+//And here is its basic implementation that only allows 'tr' inside of 'table' and 'td' inside of 'tr'
+//TABLE, TR, and TD are utility classes that shouldn't appear explicitly in the code, and
+//that’s why they’re named in capital letters
+fun implBasic() {
+  open class Tag
+
+  class TABLE : Tag() {
+    fun tr(init: TR.() -> Unit) = TR().apply(init)
+  }
+
+  class TR : Tag() {
+    fun td(init: TD.() -> Unit) = TD().apply(init)
+  }
+
+  class TD : Tag()
+}
+
+//Here is the same listing making all the receivers explicit
+fun createSimpleTableExplicit() = createHTML().
+  table {
+    (this@table).tr {
+      (this@tr).td {
+        +"cell"
+      }
+    }
+  }
+
+//Create your own HTML DSL below
+open class Tag(val name: String) {
+  private val children = mutableListOf<Tag>()
+
+  protected fun <T: Tag> doInit(child: T, init: T.() -> Unit) {
+    child.init()
+    children.add(child)
+  }
+
+  override fun toString() = "<$name>${children.joinToString("")}</$name>"
+}
+
+//creates a new table and applies the lambda
+fun table(init: TABLE.() -> Unit) = TABLE().apply(init)
+
+class TABLE : Tag("table") {
+  fun tr(init: TR.() -> Unit) = doInit(TR(), init)
+}
+
+class TR : Tag("tr") {
+  fun td(init: TD.() -> Unit) = doInit(TD(), init)
+}
+
+class TD : Tag("td")
+
+fun createTable() =
+  table {
+    tr {
+      td {
+      }
+    }
+  }
+
+fun testCreateTable(){
+  println(createTable())
+  //prints <table><tr><td></td></tr></table>
+}
+
+fun createAnotherTable() = table {
+  for (i in 1..2) {
+    tr { //each call to 'tr' creates a new TR tag and adds it to the children of TABLE
+      td {
+      }
+    }
+  }
+}
+
+fun testCreateAnotherTable() {
+  println(createAnotherTable())
+//prints <table><tr><td></td></tr><tr><td></td></tr></table>
+}
+
+//Bootstrap example in normal HTML
+//<div class="dropdown">
+//  <button class="btn dropdown-toggle">
+//    Dropdown
+//    <span class="caret"></span>
+//  </button>
+//  <ul class="dropdown-menu">
+//    <li><a href="#">Action</a></li>
+//    <li><a href="#">Another action</a></li>
+//    <li role="separator" class="divider"></li>
+//    <li class="dropdown-header">Header</li>
+//    <li><a href="#">Separated link</a></li>
+//  </ul>
+//</div>
+
+//Bootstrap example in kotlinx.html
+fun buildDropdown() = createHTML().div(classes = "dropdown") {
+  button(classes = "btn dropdown-toggle") {
+    +"Dropdown"
+    span(classes = "caret")
+  }
+  ul(classes = "dropdown-menu") {
+    li { a("#") { +"Action" } }
+    li { a("#") { +"Another action" } }
+    li { role = "separator"; classes = setOf("divider") }
+    li { classes = setOf("dropdown-header"); +"Header" }
+    li { a("#") { +"Separated link" } }
+  }
+}
+
+//Now extract them into different functions
+
+fun <R> TagConsumer<R>.dropdown(ext: DIV.() -> Unit) = div(classes = "dropdown", block = ext)
+
+fun DIV.dropdownButton(ext: BUTTON.() -> Unit) = button(classes = "btn dropdown-toggle", block = ext)
+
+fun DIV.dropdownMenu(ext: UL.() -> Unit) = ul(classes = "dropdown-menu", block = ext)
+
+fun UL.item(href: String, name:String) = li { a(href) { +name } }
+
+fun UL.divider() = li { role = "seperator"; classes = setOf("divider")}
+
+fun UL.dropdownHeader(title: String) = li { classes = setOf("dropdown-header"); +title}
+
+//And we have our own custom DSL for html components styled with bootstrap
+fun dropdownExample() = createHTML().dropdown {
+  dropdownButton { +"Dropdown" }
+  dropdownMenu {
+    item("#", "Action")
+    item("#", "Another action")
+    divider()
+    dropdownHeader("Header")
+    item("#", "Separated link")
+  }
+}
+
+fun <T> propaComponent(html: TagConsumer<T>.() -> Unit){
+  val tagConsumer = createHTML()
+  tagConsumer.html()
+}
+
+//With the invoke convention, you can call a class or object as a function
+//A class for which the invoke method with an operator modifier is defined can be called as a function.
+class Greeter(val greeting: String) {
+  operator fun invoke(name: String) {
+    println("$greeting, $name!")
+  }
+}
+//You can define it
+//with any number of parameters and with any return type, or even define multiple
+//overloads of invoke with different parameter types.
+fun testGreeter() {
+  val bavarianGreeter = Greeter("Servus")
+  bavarianGreeter("Dmitry")
+}
+
+//pg.300/327 Using function types as interfaces
+data class Issue(
+  val id: String, val project: String, val type: String,
+  val priority: String, val description: String
+)
+
+class ImportantIssuesPredicate(val project: String): (Issue) -> Boolean {
+  override fun invoke(issue: Issue): Boolean {
+    return issue.project == project && issue.isImportant()
+  }
+
+  private fun Issue.isImportant(): Boolean {
+    return type == "Bug" &&
+      (priority == "Major" || priority == "Critical")
+  }
+}
+
+fun testIssue(){
+  val i1 = Issue("IDEA-154446", "IDEA", "Bug", "Major", "Save settings failed")
+  val i2 = Issue("KT-12183", "Kotlin", "Feature", "Normal",
+    "Intention: convert several calls on the same receiver to with/apply")
+
+  val predicate = ImportantIssuesPredicate("IDEA")
+
+  for (issue in listOf(i1, i2).filter(predicate)) {
+    println(issue.id)
+  }
+  //prints IDEA-154446
+}
+
+interface Matcher<T> {
+  fun test(value: T)
+}
+
+class startWith(val prefix: String) : Matcher<String> {
+  override fun test(value: String) {
+    if (!value.startsWith(prefix))
+      throw AssertionError("String $value does not start with $prefix")
+  }
+}
+
+//remember infix calls
+infix fun <T> T.should(matcher: Matcher<T>) = matcher.test(this)
+
+fun testInfix(){
+  "kotlin" should startWith("kot")
+}
+
+//chained infix calls pg 304/331
+object start
+
+infix fun String.should(x: start): StartWrapper = StartWrapper(this)
+
+class StartWrapper(val value: String) {
+  infix fun with(prefix: String) =
+    if (!value.startsWith(prefix))
+      throw AssertionError("String does not start with $prefix: $value")
+    else Unit
+}
+
+fun testChaninedInfix(){
+  //chained infix calls with the 'start' object as a parameter
+  "kotlin".should(start).with("kot")
+
+  //can become
+  "kotlin" should start with "kot"
+}
+
+
+
+//my own test
+interface PropaComponent: (HTMLTag.() -> Unit) -> Unit{
+  override fun invoke(ext: HTMLTag.() -> Unit) {
+    (this as HTMLTag).ext()
+  }
 }
